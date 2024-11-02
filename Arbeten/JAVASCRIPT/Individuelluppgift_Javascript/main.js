337f04dfdf115eb5909fdd0280f6b871a24e1f0c
natnael_94@hotmail.com <natnael_94@hotmail.com>
2024-11-03 00:25:11 +0100

    update

--- Arbeten/JAVASCRIPT/Individuelluppgift_Javascript/main.js
+++ Arbeten/JAVASCRIPT/Individuelluppgift_Javascript/main.js
@@ -1,40 +1,24 @@
 function registerUser() 
-
 {
-
 let nameFieldType = document.getElementById('nameFieldType');
 let telephoneFieldType = document.getElementById('phoneFieldType');
-
 let toDisplayName = document.getElementById('registeredName')
 let toDisplayTelephone = document.getElementById('registeredPhone')
-
 let nameText = nameFieldType.value.trim();
 let telephoneText = telephoneFieldType.value.trim();
 
 if (nameText !== "") 
-
 {
-
-let nameListItem = document.createElement('li');
-document.getElementById('registeredName').style.display = "block";
-nameListItem.textContent = nameText;
-toDisplayName.appendChild(nameListItem);
-
+document.getElementById('registeredName').style.display = 'inherit';
+textContent = nameText;
 }
-
 if (telephoneText !== "") 
-
 {
-let phoneListItem = document.createElement('li');
-document.getElementById('registeredPhone').style.display = "block";
-phoneListItem.textContent = telephoneText;
-toDisplayTelephone.appendChild(phoneListItem);
-
+document.getElementById('registeredPhone').style.display = 'inherit';
+textContent = telephoneText;
 }
-
 nameFieldType.value = "";
 telephoneFieldType.value = "";
-
 }
 
 
