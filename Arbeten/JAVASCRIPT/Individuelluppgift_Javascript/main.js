24b8b6e4f96ea0a390b912c7f97f0d5f0caf0f39
natnael_94@hotmail.com <natnael_94@hotmail.com>
2024-11-03 21:37:46 +0100

    update

--- Arbeten/JAVASCRIPT/Individuelluppgift_Javascript/main.js
+++ Arbeten/JAVASCRIPT/Individuelluppgift_Javascript/main.js
@@ -1,24 +1,55 @@
+// input fält (namn/alias + telefon nr) & skapa knapp //
 function registerUser() 
 {
+
 let nameFieldType = document.getElementById('nameFieldType');
-let telephoneFieldType = document.getElementById('phoneFieldType');
-let toDisplayName = document.getElementById('registeredName')
-let toDisplayTelephone = document.getElementById('registeredPhone')
-let nameText = nameFieldType.value.trim();
-let telephoneText = telephoneFieldType.value.trim();
+let phoneFieldType = document.getElementById('phoneFieldType');
+let toDisplayUserName = document.getElementById('registeredName');
+let toDisplayUserPhone = document.getElementById('registeredPhone');
+let editUser = document.getElementById('ändra');
+let removeUser = document.getElementById('radera');
 
-if (nameText !== "") 
+if (nameFieldType.value !== "" && phoneFieldType.value !== "")
 {
-document.getElementById('registeredName').style.display = 'inherit';
-textContent = nameText;
+toDisplayUserName.value = nameFieldType.value;
+toDisplayUserPhone.value = phoneFieldType.value;
+toDisplayUserName.style.display = "inherit";
+toDisplayUserPhone.style.display = "inherit";
+toDisplayUserName.style.background = "lightgray";
+toDisplayUserPhone.style.background = "lightgray"
+editUser.style.display = "inherit";
+removeUser.style.display = "inherit";
 }
-if (telephoneText !== "") 
 {
-document.getElementById('registeredPhone').style.display = 'inherit';
-textContent = telephoneText;
-}
 nameFieldType.value = "";
-telephoneFieldType.value = "";
+phoneFieldType.value = "";    
+}
 }
 
+
+// redigerings knapp //
+function editUser()
+{
+let editButton = document.getElementById('ändra');
+
+if (editButton)
+{
+document.getElementById('registeredName').readOnly = false;
+document.getElementById('registeredPhone').readOnly = false;
+document.getElementById('registeredName').style.background = "white";
+document.getElementById('registeredPhone').style.background = "white";
+}
+
+
+}
+
+
+// raderings knapp //
+function removeUser() 
+{
+let toDisplayUserName = document.getElementById('registeredName')
+let toDisplayUserPhone = document.getElementById('registeredPhone')
+let removeButton = document.getElementById('ändra')
+
+}
 
