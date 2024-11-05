5bc5b4e90190e78cc9abdff74fecc384825200a2
natnael_94@hotmail.com <natnael_94@hotmail.com>
2024-11-05 07:42:00 +0100

    update

--- Arbeten/JAVASCRIPT/Individuelluppgift_Javascript/main.js
+++ Arbeten/JAVASCRIPT/Individuelluppgift_Javascript/main.js
@@ -1,14 +1,12 @@
 // input fält (namn/alias + telefon nr) & skapa knapp //
 function registerUser() 
 {
-
 let nameFieldType = document.getElementById('nameFieldType');
 let phoneFieldType = document.getElementById('phoneFieldType');
 let toDisplayUserName = document.getElementById('registeredName');
 let toDisplayUserPhone = document.getElementById('registeredPhone');
 let editUser = document.getElementById('ändra');
 let removeUser = document.getElementById('radera');
-
 // lagrande för användare //
 if (nameFieldType.value !== "" && phoneFieldType.value !== "")
 {
@@ -17,33 +15,27 @@ toDisplayUserPhone.value = phoneFieldType.value;
 toDisplayUserName.style.display = "inherit";
 toDisplayUserPhone.style.display = "inherit";
 toDisplayUserName.style.background = "lightgray";
-toDisplayUserPhone.style.background = "lightgray"
+toDisplayUserPhone.style.background = "lightgray";
 editUser.style.display = "inherit";
 removeUser.style.display = "inherit";
 }
-
 // reset på nameFieldType & phoneFieldType //
 {
 nameFieldType.value = "";
 phoneFieldType.value = "";    
 }
-
 }
 
 
 // redigerings knapp som false //
 let isEditing = false;
-
 function editUser() 
 {
-
 let editButton = document.getElementById('ändra');
 let nameField = document.getElementById('registeredName');
-let phoneField = document.getElementById('registeredPhone');
-    
+let phoneField = document.getElementById('registeredPhone');   
 // redigerings status //
 isEditing = !isEditing;
-
 if (isEditing)
 {
 // spara knapp //
@@ -53,7 +45,6 @@ nameField.style.background = "white";
 phoneField.style.background = "white";
 editButton.innerText = "Spara";
 } 
-
 else 
 {
 // redigering av knapp //
@@ -63,21 +54,15 @@ nameField.style.background = "lightgray";
 phoneField.style.background = "lightgray";
 editButton.innerText = "Ändra";
 }
-
 }
-
-
-
 
 
 // raderings knapp //
 function removeUser() 
-{
-    
+{    
 let removeButton = document.getElementById('radera')
 let removeUserName = document.getElementById('registeredName')
 let removeUserPhone = document.getElementById('registeredPhone')
-
-
+// INTE KLART //
 }
 
