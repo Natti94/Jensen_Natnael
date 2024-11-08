180b45c0109a3585890adfa901bef89b61d04869
Natti94 <natnael_94@hotmail.com>
2024-11-08 22:09:45 +0100

    update

--- Arbeten/JAVASCRIPT/Individuelluppgift_Javascript/main.js
+++ Arbeten/JAVASCRIPT/Individuelluppgift_Javascript/main.js
@@ -2,16 +2,15 @@ function registerUser() {
   let nameFieldType = document.getElementById("nameFieldType");
   let phoneFieldType = document.getElementById("phoneFieldType");
   let errorMessageNamePhone = document.getElementById("errorMessageNamePhone");
-  let errorMessageName = document.getElementById("errorMessageName")
-  let errorMessagePhone = document.getElementById("errorMessagePhone")
+  let errorMessageName = document.getElementById("errorMessageName");
+  let errorMessagePhone = document.getElementById("errorMessagePhone");
   let userList = document.getElementById("userList");
+  let removeAllMessage = document.getElementById("usersDeleted");
   errorMessageNamePhone.style.display = "none";
   errorMessageName.style.display = "none";
   errorMessagePhone.style.display = "none";
-  
-
+  removeAllMessage.style.display = "none";
   if (nameFieldType.value !== "" && phoneFieldType.value !== "") {
-    
     let userContainer = document.createElement("div");
     userContainer.style.marginBottom = "10px";
     let displayName = document.createElement("input");
@@ -34,7 +33,6 @@ function registerUser() {
     removeButton.onclick = function () {
       removeUser(userContainer);
     };
-
     userContainer.appendChild(displayName);
     userContainer.appendChild(displayPhone);
     userContainer.appendChild(editButton);
@@ -43,29 +41,32 @@ function registerUser() {
     nameFieldType.value = "";
     phoneFieldType.value = "";
     return;
-  } if (nameFieldType.value === "" && phoneFieldType.value === "") {
+  }
+  if (nameFieldType.value === "" && phoneFieldType.value === "") {
     errorMessageNamePhone.style.display = "inherit";
     return;
   }
-
   if (nameFieldType.value === "") {
     errorMessageName.style.display = "inherit";
     return;
   }
-
   if (phoneFieldType.value === "") {
     errorMessagePhone.style.display = "inherit";
     return;
   }
 }
 
-function removeAllUser() {
-  userList.remove()
-  
+function removeUser(userContainer) {
+  let removeAllMessage = document.getElementById("usersDeleted");
+  let userList = document.getElementById("userList");
+  userContainer.remove();
+  if (userList.children.length === 0) {
+    removeAllMessage.style.display = "inherit";
+  }
 }
 
-
 function editUser(nameField, phoneField, editButton) {
+  let editingError = document.getElementById("editingError");
   if (editButton.innerText === "Ändra") {
     nameField.readOnly = false;
     phoneField.readOnly = false;
@@ -73,6 +74,11 @@ function editUser(nameField, phoneField, editButton) {
     phoneField.style.background = "inherit";
     editButton.innerText = "Spara";
   } else {
+    if (nameField.value === "" || phoneField.value === "") {
+      editingError.style.display = "inherit";
+      return;
+    }
+    editingError.style.display = "none";
     nameField.readOnly = true;
     phoneField.readOnly = true;
     nameField.style.background = "lightgray";
@@ -81,6 +87,11 @@ function editUser(nameField, phoneField, editButton) {
   }
 }
 
-function removeUser(userContainer) {
-  userContainer.remove();
+function removeAllUser() {
+  let userList = document.getElementById("userList");
+  let removeAllMessage = document.getElementById("usersDeleted");
+  if (userList) {
+    userList.innerHTML = "";
+  }
+  removeAllMessage.style.display = "inherit";
 }
