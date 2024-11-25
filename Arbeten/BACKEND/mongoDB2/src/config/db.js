39443c99b941e201b394bc9abf40464895c29f7e
Natti94 <natnael_94@hotmail.com>
2024-11-25 11:00:39 +0100

    update

--- Arbeten/BACKEND/mongoDB2/src/config/db.js
+++ Arbeten/BACKEND/mongoDB2/src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_CONNECTION_STRING)
+    await mongoose.connect(process.env.DBPW)
     console.log('MongoDB connected')
   } catch (error) {
     console.error('Error connecting to MongoDB', error.message)
